3dbd5d821ae4606a04265167cbacd91dfb5bd013
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-12-23 12:39:27 +0100

    made editor more usable

--- editor.py
+++ editor.py
@@ -13,6 +13,7 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import os
 
 import ffmpeg
 
@@ -37,7 +38,7 @@ def processCuts(cuts: dict):
                         yield video.audio.filter("atrim", start=start, duration=end).filter_('asetpts', 'PTS-STARTPTS')
 
 
-def cutVideo(cuts: dict):
+def cutVideo(cuts: dict, outputFilename):
     # convert all filenames to ffmpeg-python video and audio that's properly cut
     videos = processCuts(cuts)
 
@@ -47,20 +48,37 @@ def cutVideo(cuts: dict):
     finalVideo = joined[0]
     finalAudio = joined[1]
 
-    ffmpeg.output(finalVideo, finalAudio, 'out.ignore.mp4').run()
+    ffmpeg.output(finalVideo, finalAudio, outputFilename).run()
+
+
+def promptEditor():
+    videoCuts = {}
+    while True:
+        filename = input("Filename (STOP to start converting): ")
+        if filename == "STOP" and videoCuts != {}:
+            break
+        elif not os.path.exists(filename):
+            print("This file does not exist.")
+            continue
+        tempCuts = {}
+        cutNo = 1
+        while True:
+            start = input(f"Cut {cutNo} Starting point (-1.0 for no cuts, STOP for next clip): ")
+            if start == "STOP":
+                break
+            elif float(start) == -1.0:
+                end = -1.0
+            else:
+                end = float(input(f"Cut {cutNo} Ending point (-1.0 to very end): "))
+            tempCuts[float(start)] = end
+            if float(start) == -1.0:
+                break
+            cutNo += 1
+        videoCuts[filename] = {"Cuts": tempCuts}
+    outputFilename = input("Output filename: ")
+    return videoCuts, outputFilename
 
 
 if __name__ == '__main__':
-    videoCuts = {
-        "video1.ignore.mp4": {
-            "Cuts": {
-                1.0: -1.0
-            }
-        },
-        "video2.ignore.mp4": {
-            "Cuts": {
-                2.0: 2.5
-            }
-        }
-    }
-    cutVideo(videoCuts)
+    cuts, output = promptEditor()
+    cutVideo(cuts, output)
